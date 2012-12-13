cf384642627735f603b47e3feb94263dd171f518
Philip Luppens <philip.luppens@gmail.com>
2012-12-13 10:26:34 +0100

    Corrected diffing approach.

--- src/main/java/org/nsesa/editor/gwt/core/client/diffing/DiffingManager.java
+++ src/main/java/org/nsesa/editor/gwt/core/client/diffing/DiffingManager.java
@@ -9,6 +9,7 @@ import org.nsesa.editor.gwt.core.client.event.CriticalErrorEvent;
 import org.nsesa.editor.gwt.core.client.ui.amendment.AmendmentController;
 import org.nsesa.editor.gwt.core.client.util.Scope;
 import org.nsesa.editor.gwt.core.shared.ComplexDiffCommand;
+import org.nsesa.editor.gwt.core.shared.ComplexDiffContext;
 import org.nsesa.editor.gwt.core.shared.ComplexDiffResult;
 import org.nsesa.editor.gwt.core.shared.DiffMethod;
 import org.nsesa.editor.gwt.editor.client.ui.document.DocumentEventBus;
@@ -29,6 +30,17 @@ import static org.nsesa.editor.gwt.core.client.util.Scope.ScopeValue.DOCUMENT;
 @Singleton
 @Scope(DOCUMENT)
 public class DiffingManager {
+
+    // style for BI diffing
+    public static final String originalChangeTemplate = "<span class=\"widget change highlight-diff\">{0}</span>";
+
+    // style for complex diffing
+    public static final String originalComplexChangeTemplate = "<span class=\"widget change highlight-red\">{0}</span>";
+    public static final String complexInsertTemplate = "<span class=\"widget change highlight-ins\">{0}</span>";
+    public static final String complexDeleteTemplate = "<span class=\"widget change highlight-del\">{0}</span>";
+    public static final String complexInsertNormalTemplate = "<span class=\"widget change highlight-ins-normal\">{0}</span>";
+    public static final String complexDeleteNormalTemplate = "<span class=\"widget change highlight-del-normal\">{0}</span>";
+    public static final String complexChangeTemplate = "<span class=\"widget change highlight-change\">{0}</span>";
 
     private static final Logger LOG = Logger.getLogger(DiffingManager.class.getName());
 
@@ -52,9 +64,9 @@ public class DiffingManager {
         for (final AmendmentController amendmentController : amendmentControllers) {
             ComplexDiffCommand command = null;
             if ("ep".equalsIgnoreCase(diffingType)) {
-                command = new ComplexDiffCommand(amendmentController.getOriginalContent(), amendmentController.getAmendmendContent(), amendmentController.getAmendmendContent());
+                command = new ComplexDiffCommand(amendmentController.getOriginalContent(), amendmentController.getAmendmendContent(), amendmentController.getAmendmendContent(), getComplexDiffContext());
             } else {
-                command = new ComplexDiffCommand(amendmentController.getOriginalContent(), amendmentController.getOriginalContent(), amendmentController.getAmendmendContent());
+                command = new ComplexDiffCommand(amendmentController.getOriginalContent(), amendmentController.getOriginalContent(), amendmentController.getAmendmendContent(), getComplexDiffContext());
             }
             commands.add(command);
         }
@@ -78,6 +90,10 @@ public class DiffingManager {
                 }
             }
         });
+    }
+
+    private ComplexDiffContext getComplexDiffContext() {
+        return new ComplexDiffContext(originalChangeTemplate, originalComplexChangeTemplate, complexInsertTemplate, complexDeleteTemplate, complexChangeTemplate, DiffMethod.WORD);
     }
 
 }
