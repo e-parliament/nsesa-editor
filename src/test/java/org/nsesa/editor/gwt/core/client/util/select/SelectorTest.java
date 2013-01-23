b3d9534f67db9720f6197c2719848e5da202378a
Philip Luppens <philip.luppens@gmail.com>
2013-01-23 11:02:16 +0100

    Added some selector tests. In progress.

---
+++ src/test/java/org/nsesa/editor/gwt/core/client/util/select/SelectorTest.java
@@ -0,0 +1,158 @@
+package org.nsesa.editor.gwt.core.client.util.select;
+
+import com.googlecode.gwt.test.GwtModule;
+import com.googlecode.gwt.test.GwtTest;
+import junit.framework.Assert;
+import org.junit.Test;
+import org.nsesa.editor.gwt.core.client.ui.overlay.document.AmendableWidget;
+import org.nsesa.editor.gwt.core.client.ui.overlay.document.AmendableWidgetImpl;
+
+import java.util.List;
+
+/**
+ * Date: 22/01/13 16:38
+ *
+ * @author <a href="philip.luppens@gmail.com">Philip Luppens</a>
+ * @version $Id$
+ */
+
+@GwtModule("org.nsesa.editor.gwt.editor.Editor")
+public class SelectorTest extends GwtTest {
+
+    final AmendableWidget root = new AmendableWidgetImpl() {
+        {
+            setType("rootNode");
+            setId("root");
+        }
+    };
+
+    final AmendableWidget level1 = new AmendableWidgetImpl() {
+        {
+            setType("typeA");
+            setId("id1");
+        }
+    };
+    final AmendableWidget level2 = new AmendableWidgetImpl() {
+        {
+            setType("typeB");
+            setId("id2");
+        }
+    };
+    final AmendableWidget level3 = new AmendableWidgetImpl() {
+        {
+            setType("typeC");
+            setId("id3");
+        }
+    };
+
+    final AmendableWidget level31 = new AmendableWidgetImpl() {
+        {
+            setType("typeC");
+            setId("id3-1");
+        }
+    };
+    final AmendableWidget level32 = new AmendableWidgetImpl() {
+        {
+            setType("typeC");
+            setId("id3-2");
+        }
+    };
+    final AmendableWidget level33 = new AmendableWidgetImpl() {
+        {
+            setType("typeD");
+            setId("id3-3");
+        }
+    };
+    final AmendableWidget level311 = new AmendableWidgetImpl() {
+        {
+            setType("typeC");
+            setId("id3-1-1");
+        }
+    };
+
+    {
+        root.addAmendableWidget(level1);
+        root.addAmendableWidget(level2);
+        root.addAmendableWidget(level3);
+
+        level3.addAmendableWidget(level31);
+        level3.addAmendableWidget(level32);
+        level3.addAmendableWidget(level33);
+
+        level31.addAmendableWidget(level311);
+    }
+
+
+    @Test
+    public void testXpathRootExpression() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]", root);
+        Assert.assertEquals("Ensure one node is found", 1, selection.size());
+        Assert.assertEquals("Ensure the correct node can be found", root, selection.get(0));
+    }
+
+    @Test
+    public void testXpathRootExpressionWithTrailingSlash() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/", root);
+        Assert.assertEquals("Ensure one node is found", 1, selection.size());
+        Assert.assertEquals("Ensure the correct node can be found", root, selection.get(0));
+    }
+
+
+    @Test
+    public void testXpathLevel1Expression() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/typeC[0]", root);
+        Assert.assertEquals("Ensure one node is found", 1, selection.size());
+        Assert.assertEquals("Ensure the correct node is found.", level3, selection.get(0));
+    }
+
+    @Test
+    public void testXpathLevel2Expression() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/typeC[0]/typeC[0]", root);
+        Assert.assertEquals("Ensure one node is found", 1, selection.size());
+        Assert.assertEquals("Ensure the correct node is found.", level31, selection.get(0));
+    }
+
+    @Test
+    public void testXpathLevel3Expression() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/typeC[0]/typeC[0]/typeC[0]", root);
+        Assert.assertEquals("Ensure one node is found", 1, selection.size());
+        Assert.assertEquals("Ensure the correct node is found.", level311, selection.get(0));
+    }
+
+    @Test
+    public void testXpathLevel2ExpressionWithSecondNode() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/typeC[0]/typeC[1]", root);
+        Assert.assertEquals("Ensure one node is found", 1, selection.size());
+        Assert.assertEquals("Ensure the correct node is found.", level32, selection.get(0));
+    }
+
+    @Test
+    public void testXpathLevel2ExpressionWithWildCard() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/*[0]/typeC[1]", root);
+        Assert.assertEquals("Ensure one node is found", 1, selection.size());
+        Assert.assertEquals("Ensure the correct node is found.", level32, selection.get(0));
+    }
+
+    @Test
+    public void testXpathMultipleReturnValuesWithWildCard() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/*[0]/*", root);
+        Assert.assertEquals("Ensure three nodes are found", 3, selection.size());
+        Assert.assertEquals("Ensure the correct node is found.", level31, selection.get(0));
+        Assert.assertEquals("Ensure the correct node is found.", level32, selection.get(1));
+        Assert.assertEquals("Ensure the correct node is found.", level33, selection.get(2));
+    }
+
+    @Test
+    public void testXpathMultipleReturnValuesWitType() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/*[0]/typeC", root);
+        Assert.assertEquals("Ensure two nodes are found", 2, selection.size());
+        Assert.assertEquals("Ensure the correct node is found.", level31, selection.get(0));
+        Assert.assertEquals("Ensure the correct node is found.", level32, selection.get(1));
+    }
+
+    @Test
+    public void testXpathNothingExpression() throws Exception {
+        final List<AmendableWidget> selection = new Selector().select("rootNode[0]/typeF[0]", root);
+        Assert.assertEquals("Ensure no node is found", 0, selection.size());
+    }
+}
