9a5ca88ca0ca13eded6afd669230c1ea02ee8008
Philip Luppens <philip.luppens@gmail.com>
2013-02-04 13:28:49 +0100

    Default implementation for the RTE plugin.

--- src/main/java/org/nsesa/editor/gwt/dialog/client/ui/rte/RichTextEditorPlugin.java
+++ src/main/java/org/nsesa/editor/gwt/dialog/client/ui/rte/RichTextEditorPlugin.java
@@ -1,6 +1,7 @@
 package org.nsesa.editor.gwt.dialog.client.ui.rte;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.inject.ImplementedBy;
 
 /**
  * Defines a plugin attached to a rich text editor
@@ -8,21 +9,25 @@ import com.google.gwt.core.client.JavaScriptObject;
  * Date: 9/01/13
  * Time: 11:06
  */
+@ImplementedBy(RichTextCompositePlugin.class)
 public interface RichTextEditorPlugin {
     /**
      * Return the plugin name
+     *
      * @return The plugin name
      */
     abstract String getName();
 
     /**
      * Run before initialization
+     *
      * @param editor
      */
     abstract void beforeInit(JavaScriptObject editor);
 
     /**
      * Run during initialization
+     *
      * @param editor
      */
     abstract void init(JavaScriptObject editor);
