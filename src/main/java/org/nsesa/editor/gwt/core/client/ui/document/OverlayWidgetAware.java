b6f00b93c47b06ab8f98c1b4a2f6f0c4731710ba
Philip Luppens <philip.luppens@gmail.com>
2013-04-26 10:13:16 +0200

    Added method to set an order on overlay widget aware classes.

--- src/main/java/org/nsesa/editor/gwt/core/client/ui/document/OverlayWidgetAware.java
+++ src/main/java/org/nsesa/editor/gwt/core/client/ui/document/OverlayWidgetAware.java
@@ -24,6 +24,7 @@ import org.nsesa.editor.gwt.core.client.ui.overlay.document.OverlayWidget;
  */
 public interface OverlayWidgetAware {
     void setOverlayWidget(OverlayWidget overlayWidget);
+    void setOrder(int order);
     IsWidget getView();
     IsWidget getExtendedView();
 }
