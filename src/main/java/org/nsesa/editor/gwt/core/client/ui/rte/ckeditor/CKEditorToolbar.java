9726ebc096613246de39c1320cbcdeeb412722ee
Stelian Groza <stelian.groza@gmail.com>
2013-06-13 14:06:28 +0200

    #259 hide source button when not in DEV mode

--- src/main/java/org/nsesa/editor/gwt/core/client/ui/rte/ckeditor/CKEditorToolbar.java
+++ src/main/java/org/nsesa/editor/gwt/core/client/ui/rte/ckeditor/CKEditorToolbar.java
@@ -13,6 +13,7 @@
  */
 package org.nsesa.editor.gwt.core.client.ui.rte.ckeditor;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 
 import java.util.ArrayList;
@@ -42,11 +43,14 @@ public class CKEditorToolbar {
      * find, replace, select all, source,
      * remove format, show/hide draft tool, show/hide draft attributes
      */
+    private static final ToolbarLine FIND_REPLACE_TOOLBAR_LINE = new ToolbarLine().add(Option.Find).add(Option.Replace)
+            .add(Option._).add(Option.SelectAll);
+
     public static CKEditorToolbar DEFAULT_TOOLBAR = new CKEditorToolbar()
             .add(new ToolbarLine().add(Option.Subscript).add(Option.Superscript).add(Option._))
             .add(new ToolbarLine().add(Option.Undo).add(Option.Redo).add(Option._).add(Option.SpecialChar))
-            .add(new ToolbarLine().add(Option.Find).add(Option.Replace).add(Option._).add(Option.SelectAll)
-                    .add(Option._).add(Option.Source))
+            .add(GWT.isProdMode() ? FIND_REPLACE_TOOLBAR_LINE :
+                                    FIND_REPLACE_TOOLBAR_LINE.add(Option._).add(Option.Source))
             .add(new ToolbarLine().add(Option.NsesaRemoveFormat).add(Option.NsesaToggle)
                     .add(Option.NsesaToggleAttributes).add(Option._));
 
